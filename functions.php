087945002261d96b11ea438d1c38c45a46d67571
lix <lix@lixpc-ubuntu>
2018-03-16 17:58:22 +0300

    back-end: ajax support

--- functions.php
+++ functions.php
@@ -18,19 +18,37 @@ function do_scripts() {
 }
 add_action('wp_enqueue_scripts', 'do_scripts');
 
+
+function adv_admin_js() {
+    echo '<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/fontawesome.css" integrity="sha384-q3jl8XQu1OpdLgGFvNRnPdj5VIlCvgsDQTQB6owSOHWlAurxul7f+JpUOVdAiJ5P" crossorigin="anonymous">';
+    echo '<script type="text/javascript">
+        dojoConfig = {
+                parseOnLoad: false,
+                async: true,
+                tlmSiblingOfDojo: false,
+                dojoBlankHtmlUrl: "'.get_stylesheet_directory_uri().'/blank.html",
+                wpAjaxUrl:"'.admin_url('admin-ajax.php').'",
+                wpNonce:"' . wp_create_nonce('ajax-nonce') .'"
+        };
+    </script>';
+}
+add_action('admin_head', 'adv_admin_js');
+
 function add_admin_scripts($hook){
-/*    
+/*  TODO:  
     if ("post-new.php" != $hook){
         return;
     }
  * 
  */
     global $dojo;
+    wp_enqueue_style('adm-soria', '//ajax.googleapis.com/ajax/libs/dojo/'.$dojo.'/dijit/themes/soria/soria.css' );
+    wp_enqueue_style('bootstrap', '//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css' );
+    wp_enqueue_style('adm-adm',   get_stylesheet_directory_uri(). '/js/admin.css',array('bootstrap','adm-soria'));
     wp_register_script('adm-dojo', '//ajax.googleapis.com/ajax/libs/dojo/'.$dojo.'/dojo/dojo.js', array(), $dojo, true);
-    wp_register_script('adm-adv', get_stylesheet_directory_uri(). '/js/admin.js',array('adm-dojo'),false,true);
+    wp_register_script('adm-adv',  get_stylesheet_directory_uri(). '/js/admin.js',array('adm-dojo'),false,true);
     wp_enqueue_script('adm-dojo');
     wp_enqueue_script('adm-adv');
-    wp_enqueue_style('adm-soria', '//ajax.googleapis.com/ajax/libs/dojo/'.$dojo.'/dijit/themes/soria/soria.css' );
 }
 add_action( 'admin_enqueue_scripts', 'add_admin_scripts');
 
@@ -86,24 +104,36 @@ function adv_fields_func($post){
         $city = get_post_meta( $post->ID,  'city', true );
         $district = get_post_meta( $post->ID,  'district', true );
         $price = get_post_meta( $post->ID,  'price', true );
+        $cats  = wp_get_post_categories( $post->ID );
+        $cat = (sizeof($cats) > 0) ? $cats[0] : 0;
     }
 ?>
     <input type="hidden" name="extra_fields_nonce" id="adv_extra_fields_nonce" value="<?php echo wp_create_nonce(__FILE__); ?>" />
-    <style type="text/css">
-        .adv-info{display:none;}
-        .adv-info label{display:block;margin-top: 1rem;}
-    </style>
-        
-    <div id="adv-info-pane" class="soria adv-info">
-        <h3>Местонахождение</h3>
-        <label for="adv-city">Город</label>
-        <div  data-dojo-type="dijit/form/FilteringSelect" id="adv-city" name="extra[city]" style="width:30rem;" value="<?php echo $city;?>" required></div>
-        <br />
-        <label for="adv-dis">Район</label>
-        <div  data-dojo-type="dijit/form/FilteringSelect" id="adv-dis" name="extra[district]" style="width:30rem;" value="<?php echo $district;?>" required></div>
-        <br />
-        <label for="adv-price">Стоимость</label>
-        <input data-dojo-type="dijit/form/ValidationTextBox" id="adv-price" name="extra[price]" type="text" value="<?php echo $price;?>"  required/>
+    <div id="adv-info-pane" class="container-fluid soria adv-info">
+        <div class="row">
+            <div class="col-6">
+                <h3><i class="fas fa-map-marker-alt"></i>&nbsp;Местонахождение</h3>
+                <label for="adv-city">Город</label>
+                <div  data-dojo-type="dijit/form/FilteringSelect" id="adv-city" name="extra[city]" 
+                      data-dojo-props="store:adv.stories.cities"
+                      style="width:30rem;" value="<?php echo $city;?>" 
+                      required></div>
+                <br />
+                <label for="adv-dis">Район</label>
+                <div  data-dojo-type="dijit/form/FilteringSelect" id="adv-dis" name="extra[district]"
+                      data-dojo-props="store:adv.stories.dstrc"
+                      style="width:30rem;" value="<?php echo $district;?>" required></div>
+            </div>
+        <div class="col-6">
+            <h3><i class="fas fa-ruble-sign"></i>&nbsp;Стоимость</h3>
+            <label for="adv-price">Стоимость, руб.</label>
+            <input data-dojo-type="dijit/form/CurrencyTextBox" id="adv-price" name="extra[price]" value="<?php echo $price;?>"  
+                   data-dojo-props="constraints:{fractional:false},currency:'руб.'" required />
+            <label for="adv-cat">Категория</label>
+            <div  data-dojo-type="dijit/form/FilteringSelect" id="adv-cat" name="extra[cat]" 
+                  data-dojo-props="store:adv.stories.cats"
+                  style="width:30rem;" value="<?php echo $cat;?>" required></div>
+        </div>
     </div>    
 <?php
 }
@@ -111,6 +141,80 @@ function adv_extra_fields() {
     add_meta_box('adv-add', 'Дополнительно', 'adv_fields_func', 'advs', 'normal', 'high');
 }
 add_action('add_meta_boxes', 'adv_extra_fields', 1);
+
+function add_extra_fields_update( $post_id ){
+    if ( !wp_verify_nonce($_POST['extra_fields_nonce'], __FILE__) ) return false;
+	if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE  ) return false;
+	if ( !current_user_can('edit_post', $post_id) ) return false;
+
+	if( !isset($_POST['extra']) ) return false;
+        
+	$extra = array_map('trim', $_POST['extra']);
+        
+        $post_type=$_POST['post_type'];
+        $cats = array($extra['cat']);
+        wp_set_post_categories( $post_id, $cats, false );
+        
+	foreach( $extra as $key=>$value ){
+            if( empty($value) ){
+                delete_post_meta($post_id, $key);
+                continue;
+            }
+            update_post_meta($post_id, $key, $value);
+	}
+	return $post_id;
+}
+add_action('save_post', 'add_extra_fields_update', 0);
+
+
+
+add_action('wp_ajax_info', 'adv_info_callback');
+add_action('wp_ajax_nopriv_info', 'adv_info_callback');
+function adv_info_callback(){
+    $q = $_REQUEST["q"];
+    $cat = false;
+    
+    switch ($q){
+        case "city":
+            $cat = get_category_by_slug("towns");
+            $cat = ($cat) ? $cat->cat_ID : -1;
+            break;
+        case "dstrc":
+            $cat = (int)$_REQUEST["city"];
+            break;
+        case "cats":
+            $cat = get_category_by_slug("adverts");
+            $cat = ($cat) ? $cat->cat_ID : -1;
+            break;
+    }
+    
+    $childs = get_terms( array(
+        'taxonomy' => 'category',
+        'parent'   => $cat,
+        'childless'=> false,
+        'orderby'  => 'name',
+        'hierarchical' => false,
+        'hide_empty' => false) );
+            
+            
+    //get_term_children( $cat, 'category');
+    $items  =  array();
+    foreach($childs as $cat){
+        $item = new stdClass();
+        $item->id = $cat->term_id;
+        $item->name = $cat->name;
+        $items[] = $item;
+    }
+    
+    $root = new stdClass();
+    $root->identifier = 'id';
+    $root->label = 'name';
+    $root->items = $items;
+    echo json_encode($root);
+    wp_die();    
+}       //adv_info_callback
+
+
 
 /*
 add_action('manage_shop_posts_custom_column',  'add_show_shop_columns', 10, 2);
--- js/admin.js
+++ js/admin.js
@@ -1,3 +1,5 @@
+/* global dojoConfig */
+
 require([
     "dojo/dom",
     "dojo/dom-attr",
@@ -9,9 +11,38 @@ require([
     if (!adv_pane){
         return;
     }
-    require(["dijit/form/FilteringSelect", "dijit/form/ValidationTextBox"],
-            function(){
+    require([   "dojo/data/ItemFileReadStore",
+                "dojo/on",
+                "dijit/registry",
+                "dijit/form/FilteringSelect", 
+                "dijit/form/ValidationTextBox",
+                "dijit/form/CurrencyTextBox"
+            ], function(ItemFileReadStore,on,registry){
+                
+                
+        var cityId,
+            node = dom.byId("adv-city");
+        cityId = attr.get(node,'value');
+        var adv = {stories:{}};
+        adv.stories.cities = new ItemFileReadStore({url:dojoConfig.wpAjaxUrl + "?action=info&q=city",clearOnClose:true});
+        adv.stories.dstrc  = new ItemFileReadStore({url:dojoConfig.wpAjaxUrl + "?action=info&q=dstrc&city="+cityId,clearOnClose:true});
+        adv.stories.cats   = new ItemFileReadStore({url:dojoConfig.wpAjaxUrl + "?action=info&q=cats",clearOnClose:true});
+        window["adv"] = adv;
+                
         parser.parse(adv_pane);
         style.set(adv_pane,{display:"block"});
+        var w = registry.byId("adv-city");
+        if (w){
+            var _on_city = function(city){
+                console.log('City change: ', city);
+                adv.stories.dstrc.close();
+                adv.stories.dstrc.url=dojoConfig.wpAjaxUrl + "?action=info&q=dstrc&city=" + city;
+                var w = registry.byId("adv-dis");
+                if (w){
+                    w.reset();
+                }
+            };  //_on_city
+            w.on('change', _on_city);
+        }
      });
 });
